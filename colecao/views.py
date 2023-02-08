3001b1ad660919f075a65febb7f97d546dc93e47
FerLevasseur <fernando.levasseur@outlook.com>
2023-02-08 02:35:00 -0300

    Adicionado css e configs

--- colecao/views.py
+++ colecao/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+def index(request):
+    return render(request, 'colecao/index.html')
+
+def imagem(request):
+    return render(request, 'colecao/imagem.html')
+
 # Create your views here.
